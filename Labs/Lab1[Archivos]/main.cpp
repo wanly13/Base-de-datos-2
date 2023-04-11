381fb7c9ff699eb898da7d882ba2fd6cc77b9dc9
wanly13 <wanlyobregon13@gmail.com>
2023-04-11 10:36:50 -0500

    Nuevo2

--- Labs/Lab1[Archivos]/main.cpp
+++ Labs/Lab1[Archivos]/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //
 // Created by milagros on 4/09/23.
 //
@@ -6,23 +5,10 @@
 #include "pregunta2.h"
 #include "pregunta3.h"
 #include "pregunta4.h"
-=======
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <cstring>
-#include <climits>
-#include <vector>
-#include "p1.cpp"
-#include "p2.cpp"
-#include "p3.cpp"
-#include "p4.cpp" 
->>>>>>> 5b52adbf077380b987ce820b3464dc53ddfbe8dd
 
 using namespace std;
 int main() {
 
-<<<<<<< HEAD
     //Pruebas p1
     cout<<"// -----------Pregunta 1 ----------- // \n";
     test_pregunta1();
@@ -43,31 +29,6 @@ int main() {
     cout<<"// -----------Pregunta 4 ----------- // \n";
     test_pregunta4();
     cin.ignore();
-=======
-
-int main(){
-// Aun faltas mejoras por implementar con la nueva explicacion que hizo en clase
-  // Pruebas p1
-  cout<<"// -----------Pregunta 1 ----------- // \n";
-  test_p1();
-  
-
-  // Pruebas p2
-  cout<<"// -----------Pregunta 2 ----------- // \n";  
-  test_p2();
-  
-  // Pruebas p3
-  cout<<"// -----------Pregunta 3 ----------- // \n";
-  //test_3();
- 
-  // Pruebas p4
-  cout<<"// -----------Pregunta 4 ----------- // \n";
-  test_p4();
-
-  
-  return 0;
-}
->>>>>>> 5b52adbf077380b987ce820b3464dc53ddfbe8dd
 
     return 0;
 }
